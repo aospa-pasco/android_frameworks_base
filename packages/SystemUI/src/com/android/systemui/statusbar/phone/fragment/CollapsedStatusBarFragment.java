890171836e9742f910bc7c397e282adc247fc1d6
Mello <mello.dmt+github@gmail.com>
2023-11-01 18:00:09 +0000

    Revert "SystemUI: Add tuner option to show battery percentage only"
    This reverts commit 66c3c210d9c3ccf3bfeb4ecd815146985dbbe06a.
    
    Change-Id: I72527d40d2b9c741be33c89821e90b22b7d093ef

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -45,7 +45,6 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.Dumpable;
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
-import com.android.systemui.battery.BatteryMeterView;
 import com.android.systemui.dump.DumpManager;
 import com.android.systemui.flags.FeatureFlags;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
@@ -276,8 +275,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         showClock(false);
         initOperatorName();
         initNotificationIconArea();
-        BatteryMeterView bmv = (BatteryMeterView) mStatusBar.findViewById(R.id.battery);
-        bmv.setIsStatusBar(true);
         mSystemEventAnimator =
                 new StatusBarSystemEventAnimator(mEndSideContent, getResources());
         mCarrierConfigTracker.addCallback(mCarrierConfigCallback);
